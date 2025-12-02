e3d60506fa4c6a39eabadd31ded3c94fcb8fe25f
Lucky Mehra <mehra27lucky@gmail.com>
2025-12-01 20:08:30 -0600

    fix color and font size

--- visualizations.py
+++ visualizations.py
@@ -10,40 +10,49 @@ def plot_monthly_breakdown(expense_df):
 
     expense_df["Date"] = pd.to_datetime(expense_df["Date"])
 
-    current_year_df = (
-        expense_df
-        .assign(
-            year=expense_df["Date"].dt.year,
-            month=expense_df["Date"].dt.strftime("%b"),
-            )
-        .query("year == @pd.Timestamp.now().year")
-    )
+    current_year_df = expense_df.assign(
+        year=expense_df["Date"].dt.year,
+        month=expense_df["Date"].dt.strftime("%b"),
+    ).query("year == @pd.Timestamp.now().year")
 
-    month_lst = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    month_lst = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ]
     current_year_df["month"] = pd.Categorical(
         current_year_df["month"], categories=month_lst, ordered=True
     )
 
     month_category = (
-        current_year_df.groupby(
-            ["month", "Category"]
-        )["Amount"]
-        .sum()
+        current_year_df.groupby(["month", "Category"])["Amount"].sum().reset_index()
+    )
+
+    monthly_sum = (
+        month_category.groupby("month")
+        .agg(monthly_sum=("Amount", lambda x: x.sum().round()))
         .reset_index()
     )
 
-    monthly_sum = month_category.groupby("month").agg(
-        monthly_sum=("Amount", lambda x: x.sum().round())   
-    ).reset_index()  
-
-    fig = px.bar(  
+    fig = px.bar(
         month_category,
         x="Amount",
         y="month",
         labels={"month": "Month", "Amount": "Total Expense"},
         color="Category",
-        orientation="h",)
-    
+        orientation="h",
+        color_discrete_sequence=px.colors.qualitative.Safe,
+    )
+
     fig.add_trace(
         go.Scatter(
             x=monthly_sum["monthly_sum"],
@@ -54,5 +63,9 @@ def plot_monthly_breakdown(expense_df):
             showlegend=False,
         )
     )
+
+    fig.update_layout(font=dict(family="Arial, sans-serif", size=14, color="black"))
+
+    fig.update_xaxes(range=[0, 10000])
 
     return fig
